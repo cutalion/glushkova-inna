6687be1b9c096bdb01760e5176d60238fafc34f2
Alexander Glushkov <cutalion@gmail.com>
2012-03-06 17:18:38 +0200

    Added basic meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
+# encoding: utf-8
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_default_meta_tags
+
+  protected
+
+  def set_default_meta_tags
+    set_meta_tags :site => 'Инна Глушкова. Фотограф', 
+                  :keywords => 'инна глушкова, фотограф, детское фото, семейное фото, freezelight, севастополь',
+                  :description => 'Инна Глушкова - фотограф из Севастополя. Детское, семейное, свадебное фото. Freezelight.'
+  end
 end
