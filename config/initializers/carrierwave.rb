6b89841873a808888e0cefa2e707742f797b5494
Alexander Glushkov <cutalion@gmail.com>
2012-03-22 18:52:25 +0200

    Secrets

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,26 @@
+require 'carrierwave/processing/mime_types'
+
+CarrierWave.configure do |config|
+  config.root = File.join Rails.root, "public"
+  config.fog_credentials = {
+    :provider               => 'AWS',       # required
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],       # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],       # required
+    :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = 'glushkova-me'                     # required
+  # config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
+  config.fog_public     = true                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+
+  case Rails.env
+  when "test" then
+    config.storage = :file
+    config.enable_processing = false
+
+  when "production" then
+    config.fog_directory  = 'glushkova-me'                     # required
+  else
+    config.fog_directory  = 'glushkova-me-dev'                     # required
+  end
+end
